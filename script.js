ef2097eec9e81de67a0b41c81980f1d6ad7973c3
The Greatest Giant <seanalter1@gmail.com>
2023-02-28 18:22:27 -0500

    capitals

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-document.addEventListener("DOMContentLoaded", async function() {
-    const data = await (await fetch("source.json")).json();
-    const page = window.document.title.split(" ").join("");
-    for (const key in data[page]) {
-      const elem = document.createElement(key.split('-')[0]);
-      elem.textContent = data[page][key];
-      document.querySelector("#content").appendChild(elem);
-    }
-    for (const pageKey of Object.keys(data)) {
-      const a = document.createElement("a");
-      a.href = pageKey.toLowerCase() === "home" ? "./" : pageKey === "Files" ? "//files.drshacker.systems" : `${pageKey.toLowerCase()}.html`;
-      a.textContent = pageKey.replace(/(\d)/g, " $1").trim();
-      document.querySelector("#navbar").appendChild(a);
-    }
-  });
+document.addEventListener("DOMContentLoaded", async function () {
+  const data = await (await fetch("source.json")).json();
+  const page = window.document.title.split(" ").join("");
+  for (const key in data[page]) {
+    const elem = document.createElement(key.split("-")[0]);
+    elem.textContent = data[page][key];
+    document.querySelector("#content").appendChild(elem);
+  }
+  for (const pageKey of Object.keys(data)) {
+    const a = document.createElement("a");
+    a.href =
+      pageKey.toLowerCase() === "home"
+        ? "./"
+        : pageKey === "Files"
+        ? "//files.drshacker.systems"
+        : `${pageKey.toLowerCase()}.html`;
+    a.textContent = pageKey
+      .replace(/(?<!^)(?=[A-Z])/g, " ")
+      .replace(/(\d)/g, " $1")
+      .trim();
+    document.querySelector("#navbar").appendChild(a);
+  }
+});
